55447e1f316abded65ee4b58fa6b365b167a19e7
Jake Aringay <jakeraymund.aringay@gmail.com>
2021-03-08 22:33:07 +1100

    Add comments in scripts.js

--- scripts.js
+++ scripts.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
   let language = "en-US";
 
   $('#searchBtn').click(() => {
-    let query = $('#searchBar').val()
+    let query = $('#searchBar').val() // gets the value of the input text where the title keyword of a movie is typed in
     $.getJSON(api + '?api_key=' + apiKey + '&language=' + language + '&query=' + query)
     .done(data => {
-      console.log(data)
+      console.log(data) // this is only to see if the API call is working (can be seen in chrome dev tools)
       $.each(data.results, (i, item) => {
-        $('#list').append('<li>' + item.title + '</li>')
+        $('#list').append('<li>' + item.title + '</li>') // See README.md for a (hopefully) comprehensive explanation
       })
     })
   })
